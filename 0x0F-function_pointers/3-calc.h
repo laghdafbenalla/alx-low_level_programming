76045ebf110074c4e0424cc19885bc1c4c9bc41b
‘laghdaf <laghdaf.benalla@gmail.com>
2023-04-21 14:50:10 -0700

    3-print_all.c

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,13 +1,9 @@
-#ifndef _CALC_H_
-#define _CALC_H_
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef CALC_H
+#define CALC_H
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
 typedef struct op
 {
@@ -22,6 +18,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif 
-
-
+#endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,13 @@
- #include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
 /**
- * get_op_func - identify function
- * @s: funct
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
  *
- * Return: int
- *
- **/
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -16,22 +16,13 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}
+		{NULL, NULL},
 	};
-	int i;
 
-	i = 0;
-	while (i < 5)
-	{
-		if (s[0] == ops[i].op[0])
-		{
-			return (ops[i].f);
-		}
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
 
-
-	return (NULL);
+	return (ops[i].f);
 }
-
-
