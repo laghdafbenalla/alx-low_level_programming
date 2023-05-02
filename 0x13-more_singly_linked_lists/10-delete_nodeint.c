a8ae393ece43fb60964565a3b726aee7f8ce0e17
‘laghdaf <laghdaf.benalla@gmail.com>
2023-05-02 16:41:29 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,37 +1,33 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * delete_nodeint_at_index - deletes the node at index
- * @head: double pointer
- * @index: index of node
- *
- * Return: pointer to the index node
- */
+  * delete_nodeint_at_index - delete a node at index
+  * @head: head of the list
+  * @index: location of the node to delete
+  * Return: pointer to head of list
+  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *temp, *next;
+	listint_t *current, *subsequent;
 
-	if (head == NULL || *head == NULL)
+	if (!head || !*head)
 		return (-1);
+	current = *head;
 	if (index == 0)
 	{
-		next = (*head)->next;
-		free(*head);
-		*head = next;
+		*head = (*head)->next;
+		free(current);
 		return (1);
 	}
-	temp = *head;
-	for (i = 0; i < index - 1; i++)
+	for (i = 0; i < (index - 1); i++)
 	{
-		if (temp->next == NULL)
+		current = current->next;
+		if (current == NULL)
 			return (-1);
-		temp = temp->next;
 	}
-	next = temp->next;
-	temp->next = next->next;
-	free(next);
+	subsequent = current->next;
+	current->next = subsequent->next;
+	free(subsequent);
 	return (1);
-
 }
